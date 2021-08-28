2a7f17c3ec2761cf7dc49b4929a6f9dd7a6c53d9
Yamangulov Andrey <yamangulov@gmail.com>
2021-08-28 17:03:00 +0300

    Initial commit. Первые изменения в проекте соответствуют видео курса Spring framework Skillbox и сделаны вслед за преподавателем.

---
+++ src/main/java/com/yamangulov/bookshop/controller/MainPageController.java
@@ -0,0 +1,30 @@
+package com.yamangulov.bookshop.controller;
+
+import com.yamangulov.bookshop.data.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Andrey.Yamangulov
+ * Date: 28.08.2021
+ * Time: 12:18
+ */
+@Controller
+@RequestMapping("/bookshop")
+public class MainPageController {
+    private final BookService bookService;
+
+    @Autowired
+    public MainPageController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping("/main")
+    public String mainPage(Model model) {
+        model.addAttribute("bookData", bookService.getBooksData());
+        return "index";
+    }
+}
